6bf20eae80cd441053b06f1234d57b571eb47148
tanish699 <tanishwadhwa2310@gmail.com>
2025-02-19 22:23:19 +0530

    getting started with Doubly Linked List

---
+++ Doubly_Linked_List/DoublyLinkedList.java
@@ -0,0 +1,71 @@
+// package Doubly_Linked_List;
+
+public class DoublyLinkedList {
+    
+    private Node head;
+    private Node tail;
+    private int length;
+    class Node{  
+        int value;
+        Node next; 
+        Node prev;
+        Node(int value){
+            this.value = value;
+            
+        }
+    }
+
+    public DoublyLinkedList(int value){
+        Node newNode = new Node(value);
+        head = newNode;
+        tail = newNode;
+        length=1;
+    }
+ 
+/*---------------------------------------------------------------------------------------------------------------*/
+    //To print linked list
+    public void print(){
+        Node temp = head;
+        while(temp != null){
+            System.out.println(temp.value);
+            temp = temp.next;
+        }
+
+    }
+    
+    //To print value of head
+    public void getHead() {
+        System.out.println("Head: " + head.value);
+    }
+
+    //To print value of Tail
+    public void getTail() {
+        System.out.println("Tail: " + tail.value);
+    }
+
+    //To print Length of Linked list
+    public void getLength() {
+        System.out.println("Length: " + length);
+    }    
+
+/*---------------------------------------------------------------------------------------------------------------*/
+    //To append a node in doubly linked list
+    public void append(int value){
+	    Node newNode = new Node(value);
+	    if(length == 0){
+	        head = newNode;
+	        tail = newNode;
+	    } else {
+            tail.next = newNode;
+	        newNode.prev = tail;
+	        tail = newNode;
+        }
+	    
+	    length++;
+	}
+
+    public static void main(String[] args) {
+        System.out.println("Code Runed Sucessfully");
+    }
+}
+
---
+++ Doubly_Linked_List/Main_DD.java
@@ -0,0 +1,11 @@
+// package Doubly_Linked_List;
+
+public class Main_DD {
+    public static void main(String[] args) {
+        DoublyLinkedList myDDL = new DoublyLinkedList(8);
+        myDDL.print();
+        System.out.println("-----------");
+        myDDL.append(9);
+        myDDL.print();
+    }
+}
